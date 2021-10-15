c2e8d3d5e2ce53e69b6b32c753b5b2ad53f997d0
Adam Busbin <adam.busbin@ingeniux.com>
2021-10-15 14:54:34 -0700

    An example upload using RESTSharp

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using RestSharp;
+using System;
+
+namespace RESTAssetUploadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://your.cms.com/api/assets/upload");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "Bearer YourBearerToken");
+            request.AddParameter("parentfolder", "af/105");
+            request.AddParameter("updateonconflicts", "true");
+            request.AddParameter("schemadefault", "false");
+            request.AddParameter("overrideschemaid", "AssetSchemas/1");
+            request.AddFile("file", @"C:\Path\to\your\file\test.dita");
+            request.AddParameter("assetname", "test");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
